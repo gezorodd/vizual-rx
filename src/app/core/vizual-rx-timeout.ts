465e5b6be513317408c5bea8bbaf1f6ce210e493
gz <geoffrey.zoroddu@gmail.com>
2024-02-14 23:16:06 +0100

    Removed unused file

--- src/app/core/vizual-rx-timeout.ts
+++
@@ -1,7 +0,0 @@
-import {Observable, ObservableInput, ObservedValueOf, OperatorFunction, timeout, TimeoutConfig} from "rxjs";
-import {vizualRxScheduler} from "./vizual-rx-scheduler";
-
-
-
-
-// export const vizualRxTimeout = timeout;
